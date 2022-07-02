9c7c5b24cac97d996e20ad349e33d943d6180190
Dhruv Reddy <dhruvr931@gmail.com>
2022-07-02 13:19:05 -0700

    Initial Commit

---
+++ M2L4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M2L4
+//
+//  Created by Dhruv Reddy on 7/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M2L4/M2L4App.swift
@@ -0,0 +1,17 @@
+//
+//  M2L4App.swift
+//  M2L4
+//
+//  Created by Dhruv Reddy on 7/2/22.
+//
+
+import SwiftUI
+
+@main
+struct M2L4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
